270e2f593b325e1ae1d50d77d22f85ea39532659
Douglas José Rosa <douglasjrosa@gmail.com>
2021-10-24 20:40:05 -0300

    Favicon via _next

--- components/elements/head.js
+++ components/elements/head.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
 
 const AppHead = (props) => {
-
+    // const {favicon} = props;
+    const favicon = "http://localhost:3000/_next/image?url=https%3A%2F%2Frbx-backend-media.s3.sa-east-1.amazonaws.com%2Flogotipo_ribermax_45x45_min_2b9f9d1ee6.webp&w=640&q=75";
     /* Favicon */
     return (
         <Head>
-           <link rel="shortcut icon" href={props.favicon} />
+           <link rel="shortcut icon" href={favicon} />
         </Head>
     )
 }
