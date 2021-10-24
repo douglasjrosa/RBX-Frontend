c7fb13783550796a6e8b8b24025d02f0f8f35a98
Douglas José Rosa <douglasjrosa@gmail.com>
2021-10-24 18:22:39 -0300

    Background Porto de Santos

--- components/layout.js
+++ components/layout.js
@@ -3,29 +3,30 @@ import Footer from "./elements/footer";
 import NotificationBanner from "./elements/notification-banner";
 import { useState } from "react";
 
-
 const Layout = ({ children, global }) => {
   const { navbar, footer, notificationBanner } = global;
 
   const [bannerIsShown, setBannerIsShown] = useState(true);
 
   return (
-    <div className="flex flex-col justify-between min-h-screen bg-gray-200 bg-rbx-forest bg-fixed bg-center bg-cover">
-      {/* Aligned to the top */}
-      <div className="flex-1">
-        <div className="fixed w-full z-50">
-          <Navbar navbar={navbar} />
+    <div className="min-h-screen bg-rbx-porto bg-fixed bg-center bg-cover">
+      <div className="flex flex-col justify-between min-h-screen bg-white bg-opacity-30">
+        {/* Aligned to the top */}
+        <div className="flex-1">
+          <div className="fixed w-full z-50">
+            <Navbar navbar={navbar} />
+          </div>
+          <div className="my-36">{children}</div>
         </div>
-        <div className="my-36">{children}</div>
+        {/* Aligned to the bottom */}
+        <Footer footer={footer} />
+        {notificationBanner && bannerIsShown && (
+          <NotificationBanner
+            data={notificationBanner}
+            closeSelf={() => setBannerIsShown(false)}
+          />
+        )}
       </div>
-      {/* Aligned to the bottom */}
-      <Footer footer={footer} />
-      {notificationBanner && bannerIsShown && (
-        <NotificationBanner
-          data={notificationBanner}
-          closeSelf={() => setBannerIsShown(false)}
-        />
-      )}
     </div>
   );
 };
