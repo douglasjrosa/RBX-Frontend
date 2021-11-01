05a0ef76ac1a831929c39dfc6da73ff3e8c986d1
Douglas José Rosa <douglasjrosa@gmail.com>
2021-11-01 20:26:26 -0300

    Favicon fixed

--- components/elements/head.js
+++ components/elements/head.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 const AppHead = (props) => {
     // const {favicon} = props;
-    const favicon = "http://localhost:3000/_next/image?url=https%3A%2F%2Frbx-backend-media.s3.sa-east-1.amazonaws.com%2Flogotipo_ribermax_45x45_min_2b9f9d1ee6.webp&w=640&q=75";
+    const favicon = "https://ribermax.com.br/_next/image?url=https%3A%2F%2Frbx-backend-media.s3.sa-east-1.amazonaws.com%2Flogotipo_ribermax_45x45_min_2b9f9d1ee6.webp&w=640&q=75";
     /* Favicon */
     return (
         <Head>
