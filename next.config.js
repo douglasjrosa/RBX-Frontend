bb15bbfc7e3e37f1f482e316c8dfaf5af6c32706
Douglas José Rosa <douglasjrosa@gmail.com>
2022-05-15 17:12:54 -0300

    Teste 5 cache

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   images: {
     domains: ["rbx-backend-media.s3.sa-east-1.amazonaws.com"],
-    minimumCacheTTL: 15552000,
-	"Cache-Control": 'public, max-age=31536000, immutable'
+    minimumCacheTTL: 15552000
+  },
+  async headers() {
+    return [
+      {
+        source: '*',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, immutable',
+          }
+        ],
+      },
+    ]
   },
 };
