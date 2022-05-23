48584c3f95e55a47403c4c3e2489869cc09499b4
Douglas José Rosa <douglasjrosa@gmail.com>
2022-05-23 00:14:09 -0300

    next.config.js commit Ok.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
 	reactStrictMode: true,
 	images: {
 		domains: ['rbx-backend-media.s3.sa-east-1.amazonaws.com'],
@@ -19,25 +18,4 @@ module.exports = {
 			}
 		];
 	}
-=======
-  images: {
-    domains: ["rbx-backend-media.s3.sa-east-1.amazonaws.com"],
-    minimumCacheTTL: 315360000,
-    disableStaticImages: true
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=315360000, immutable',
-          }
-        ],
-      },
-    ]
-  },
->>>>>>> 7a17585de6cdeed51efbc2661231ef2c185a9ca4
 };
