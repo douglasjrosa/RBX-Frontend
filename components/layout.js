6f8b9b720d184997fb92903fd7c9af91d6c1fc12
Douglas José Rosa <douglasjrosa@gmail.com>
2022-05-27 22:12:23 -0300

    voltando o bg para fora do css para testar

--- components/layout.js
+++ components/layout.js
@@ -13,39 +13,48 @@ const Layout = ({ children, global }) => {
 		whatsappImage,
 		whatsappContacts,
 		whatsappMsg
-	} = global;	
+	} = global;
 
 	const [bannerIsShown, setBannerIsShown] = useState(true);
 
 	return (
 		<div>
-			<div className="bg-rbx-wood bg-fixed bg-repeat">
-				<div className='fixed w-full h-full bg-white opacity-10 z-0' ></div>
-				<div className="flex flex-col justify-between ">
-
-				{/* Aligned to the top */}
-				<div className="flex-1">
-					<div className="fixed w-full z-50">
-						<Navbar navbar={navbar} />
-					</div>
-					<div className="relative my-36 z-10">{children}</div>
+			<div>
+				<div className="min-w-full min-h-screen fixed">
+					<Image
+						media={{
+							name: 'porto de santos.jpg'
+						}}
+						layout="fill"
+					/>
 				</div>
-				{/* Aligned to the bottom */}
-				<Footer footer={footer} />
-				{whatsappImage && whatsappContacts.length && whatsappMsg && (
-					<WhatsAppButton
-					media={whatsappImage}
-					contatos={whatsappContacts}
-					msg={whatsappMsg}
-					/>
-					)}
-				{notificationBanner && bannerIsShown && (
-					<NotificationBanner
-					data={notificationBanner}
-					closeSelf={() => setBannerIsShown(false)}
-					/>
-					)}
+				<div className="fixed w-full min-h-screen bg-black opacity-50 z-0"></div>
+				<div className="flex flex-col justify-between ">
+					{/* Aligned to the top */}
+					<div className="flex-1">
+						<div className="fixed w-full z-50">
+							<Navbar navbar={navbar} />
+						</div>
+						<div className="relative my-36 z-10">{children}</div>
 					</div>
+					{/* Aligned to the bottom */}
+					<Footer footer={footer} />
+					{whatsappImage &&
+						whatsappContacts.length &&
+						whatsappMsg && (
+							<WhatsAppButton
+								media={whatsappImage}
+								contatos={whatsappContacts}
+								msg={whatsappMsg}
+							/>
+						)}
+					{notificationBanner && bannerIsShown && (
+						<NotificationBanner
+							data={notificationBanner}
+							closeSelf={() => setBannerIsShown(false)}
+						/>
+					)}
+				</div>
 			</div>
 		</div>
 	);
