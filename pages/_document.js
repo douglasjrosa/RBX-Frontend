fa7c62a478b9cd7f3641e5cdf93fb99f8b35ba2f
Douglas José Rosa <douglasjrosa@gmail.com>
2022-06-03 17:58:47 -0300

    Disabling scripts in production env

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,37 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  
+class HeadProduction extends Head {
   render() {
-
+    const { head } = this.context._documentProps;
+    const children = this.props.children;
     return (
-      <Html lang="pt-BR">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+      <head {...this.props}>
+        {children}
+        {head}
+        {this.getCssLinks()}
+      </head>
+    );
   }
 }
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const isDev = process.env.NODE_ENV === "development";
+    return (
+      <Html>
+        {isDev ? <Head /> : <HeadProduction />}
+        <body>
+          <Main />
+          {isDev && <NextScript />}
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
