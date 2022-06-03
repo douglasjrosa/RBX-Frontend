e75f8fe48406e307b717c7ff328e7e5f068326fc
Douglas José Rosa <douglasjrosa@gmail.com>
2022-06-03 19:56:32 -0300

    Test hibrid amp site implementation

--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-class HeadProduction extends Head {
+export default class MyDocument extends Document {
 	render() {
-		const { head } = this.context._documentProps;
-		const children = this.props.children;
 		return (
-			<head {...this.props}>
-				{children}
-				{head}
-				{this.getCssLinks()}
-			</head>
-		);
-	}
-}
-
-class MyDocument extends Document {
-	render() {
-		const isDev = process.env.NODE_ENV === 'development';
-		return (
-			<Html>
-				{isDev ? <Head /> : <HeadProduction />}
+			<Html lang="pt-BR">
+				<Head />
 				<body>
 					<Main />
-					{isDev && <NextScript />}
+					<NextScript />
 				</body>
 			</Html>
 		);
 	}
 }
-
-export default MyDocument;
