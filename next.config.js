8fe2e27cd21eb89db3e16f7c294c787a886493d5
Douglas José Rosa <douglasjrosa@gmail.com>
2022-06-12 19:21:28 -0300

    Next bundle analyzer working

--- next.config.js
+++ next.config.js
@@ -1,34 +1,39 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true'
+});
 const withPWA = require('next-pwa');
 
-module.exports = withPWA({
-	pwa: {
-		dest: 'public',
-		register: true,
-		skipWaiting: true,
-		publicExcludes: ['!images/*'],
-		buildExcludes: [/chunks\/images\/.*$/, /images\/.*$/, /media\/.*$/],
-		disable: process.env.NODE_ENV === 'development'
-	},
-	productionBrowserSourceMaps: true,
-	images: {
-		domains: ['rbx-backend-media.s3.sa-east-1.amazonaws.com'],
-		minimumCacheTTL: 15552000,
-		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-		formats: ['image/avif', 'image/webp']
-	},
-	async headers() {
-		return [
-			{
-				source: '/(.*)',
-				locale: false,
-				headers: [
-					{
-						key: 'Cache-Control',
-						value: 'public, max-age=9999999999, immutable'
-					}
-				]
-			}
-		];
-	}
-});
+module.exports = withBundleAnalyzer(
+	withPWA({
+		pwa: {
+			dest: 'public',
+			register: true,
+			skipWaiting: true,
+			publicExcludes: ['!images/*'],
+			buildExcludes: [/chunks\/images\/.*$/, /images\/.*$/, /media\/.*$/],
+			disable: process.env.NODE_ENV === '!development'
+		},
+		productionBrowserSourceMaps: true,
+		images: {
+			domains: ['rbx-backend-media.s3.sa-east-1.amazonaws.com'],
+			minimumCacheTTL: 15552000,
+			deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+			imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+			formats: ['image/avif', 'image/webp']
+		},
+		async headers() {
+			return [
+				{
+					source: '/(.*)',
+					locale: false,
+					headers: [
+						{
+							key: 'Cache-Control',
+							value: 'public, max-age=9999999999, immutable'
+						}
+					]
+				}
+			];
+		}
+	})
+);
