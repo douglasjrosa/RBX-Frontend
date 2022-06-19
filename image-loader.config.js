fc68e4ce4d4ea8691cf914da09fab193e28f6e7a
Douglas José Rosa <douglasjrosa@gmail.com>
2022-06-19 07:10:39 -0300

    Test TBT with next-image-loader

--- image-loader.config.js
+++ image-loader.config.js
@@ -1,6 +1,6 @@
 import { imageLoader } from 'next-image-loader/build/image-loader';
 
 imageLoader.loader = ({ src, width, quality }) =>
-	`${process.env.NEXT_PUBLIC_OPTIMIZE_DOMAIN}?url=${encodeURIComponent(
+	`${window.location.origin}?url=${encodeURIComponent(
 		src
 	)}&w=${Math.min(width, 1080)}&q=${quality || 75}`;
