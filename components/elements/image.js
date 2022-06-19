452f76bfd9f7d1b2aafe9dcd2a21ac8e8d6c7b5c
Douglas José Rosa <douglasjrosa@gmail.com>
2022-06-19 11:26:08 -0300

    Test TBT with NEXT_PUBLIC_ env vars

--- components/elements/image.js
+++ components/elements/image.js
@@ -2,7 +2,7 @@ import NextImage from 'next/image';
 
 const customLoader = ({ src, width, quality }) => {
 	return `${process.env.NEXT_PUBLIC_WEB_PROTOCOL}://${
-		process.env.NEXT_PUBLIC_SITE_URL
+		process.env.NEXT_PUBLIC_SITE_DOMAIN
 	}/_next/image?url=${encodeURIComponent(src)}&w=${Math.min(width, 1080)}&q=${
 		quality || 75
 	}`;
