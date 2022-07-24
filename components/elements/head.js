cf1ef7be2f5659b837fb1011a4e6e846a612651b
Douglas José Rosa <douglasjrosa@gmail.com>
2022-07-24 15:34:29 -0300

    No amp

--- components/elements/head.js
+++ components/elements/head.js
@@ -1,11 +1,8 @@
 import Head from 'next/head';
-import { useAmp } from 'next/amp';
 
 const AppHead = () => {
-	const isAmp = useAmp();
 	return (
 		<Head>
-			<meta isAmp={isAmp} />
 			<link rel="manifest" href="/manifest.json" />
 			<link rel="apple-touch-icon" href="/icon.png"></link>
 			<meta name="theme-color" content="#8b572a" />
