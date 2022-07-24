ac86e5eb1d3e4b5cf660be2dd8d3697e18220cbc
Douglas José Rosa <douglasjrosa@gmail.com>
2022-07-24 13:46:44 -0300

    no layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,42 @@
-import Layout from '@/components/layout';
+import { DefaultSeo } from 'next-seo';
+import '@/styles/index.css';
+import { useRouter } from 'next/router';
+import Loading from '@/components/elements/loading';
+import {metaTitleSuffix, metadata} from 'data/global'
 
 const MyApp = ({ Component, pageProps }) => {
 	// Prevent Next bug when it tries to render the [[...slug]] route
+	const router = useRouter();
+	if (router.asPath === '/[[...slug]]') return <Loading />;
 	
 	return (
 		<>
-			<Layout>
+			{/* Global site metadata */}
+			<DefaultSeo
+				titleTemplate={`%s | ${metaTitleSuffix}`}
+				title={'Page'}
+				description={metadata.metaDescription}
+				openGraph={{
+					images: Object.values(metadata.shareImage.formats).map(
+						(image) => {
+							return {
+								url: image.url,
+								width: image.width,
+								height: image.height
+							};
+						}
+					)
+				}}
+				twitter={{
+					cardType: metadata.twitterCardType,
+					handle: metadata.twitterUsername
+				}}
+			/>
+			{/* Display the content */}
+
+			<div>
 				<Component {...pageProps} />
-			</Layout>
+			</div>
 		</>
 	);
 };
