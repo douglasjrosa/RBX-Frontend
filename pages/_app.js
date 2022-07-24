8cec2ff7cfcd627f62ca5f5597855ca9cce11e88
Douglas José Rosa <douglasjrosa@gmail.com>
2022-07-24 13:41:29 -0300

    No css, useRouter and Lading

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,9 @@
 import { DefaultSeo } from 'next-seo';
 import Layout from '@/components/layout';
-import '@/styles/index.css';
-import { useRouter } from 'next/router';
-import Loading from '@/components/elements/loading';
 import {metaTitleSuffix, metadata} from 'data/global'
 
 const MyApp = ({ Component, pageProps }) => {
 	// Prevent Next bug when it tries to render the [[...slug]] route
-	const router = useRouter();
-	if (router.asPath === '/[[...slug]]') return <Loading />;
 	
 	return (
 		<>
