e7bf87d47c32bf4638492a2bb92f0745b9df08ce
Douglas José Rosa <douglasjrosa@gmail.com>
2022-07-24 17:23:31 -0300

    No bg-rbx-wood

--- components/elements/main-container.js
+++ components/elements/main-container.js
@@ -1,23 +1,16 @@
-import { useState, useEffect } from 'react';
+const Container = ({children, slug}) => {
+	
+	const mainPages = [
+		'',
+		'empresa',
+		'produtos',
+		'informacoes',
+		'contato'
+	];
+	const theme = mainPages.includes(slug)
+		? 'bg-rbx-porto bg-fixed bg-cover py-36'
+		: 'bg-[#E3C486] bg-repeat py-36';
 
-const Container = ({ children, slug }) => {
-	const [mounted, setMounted] = useState(false);
-
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	const mainPages = ['', 'empresa', 'produtos', 'informacoes', 'contato'];
-	const theme = mounted
-		? mainPages.includes(slug)
-			? 'bg-rbx-porto bg-fixed bg-cover py-36'
-			: 'bg-rbx-wood bg-repeat py-36'
-		: 'bg-[#E3C486] py-36';
-
-	return (
-		<div id="main-container" className={theme}>
-			{children}
-		</div>
-	);
-};
+	return <div id="main-container" className={theme} >{children}</div>
+}
 export default Container;
