30bc68245eec5fde42a62f20700c1c13559b98b1
Douglas José Rosa <douglasjrosa@gmail.com>
2022-07-24 13:42:19 -0300

    no defaultSeo

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,10 @@
-import { DefaultSeo } from 'next-seo';
 import Layout from '@/components/layout';
-import {metaTitleSuffix, metadata} from 'data/global'
 
 const MyApp = ({ Component, pageProps }) => {
 	// Prevent Next bug when it tries to render the [[...slug]] route
 	
 	return (
 		<>
-			{/* Global site metadata */}
-			<DefaultSeo
-				titleTemplate={`%s | ${metaTitleSuffix}`}
-				title={'Page'}
-				description={metadata.metaDescription}
-				openGraph={{
-					images: Object.values(metadata.shareImage.formats).map(
-						(image) => {
-							return {
-								url: image.url,
-								width: image.width,
-								height: image.height
-							};
-						}
-					)
-				}}
-				twitter={{
-					cardType: metadata.twitterCardType,
-					handle: metadata.twitterUsername
-				}}
-			/>
-			{/* Display the content */}
-
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
