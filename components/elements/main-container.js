94e0159ba65129d8d012e49b573d85ad8a3292b8
Douglas José Rosa <douglasjrosa@gmail.com>
2022-07-23 22:43:04 -0300

    Hello World TBT teste page

--- components/elements/main-container.js
+++ components/elements/main-container.js
@@ -9,7 +9,7 @@ const Container = ({children, slug}) => {
 	];
 	const theme = mainPages.includes(slug)
 		? 'bg-rbx-porto bg-fixed bg-cover py-36'
-		: 'bg-rbx-green bg-[length:50px_50px] py-36';
+		: 'bg-rbx-wood bg-repeat py-36';
 
 	return <div id="main-container" className={theme} >{children}</div>
 }
--- pages/teste.js
+++ pages/teste.js
@@ -1,27 +1,7 @@
-import ErrorPage from 'next/error';
-import Sections from '@/components/sections';
-import Seo from '@/components/elements/seo';
-import { useRouter } from 'next/router';
-import Loading from '@/components/elements/loading';
-import {page} from '@/lib/pages/embalagem-padrao-exportacao'
-
 export const config = { amp: 'hybrid' };
 
 const Teste = () => {
-	const router = useRouter();
-
-	if (router.isFallback) return <Loading />;
-	if (!page) return <ErrorPage statusCode={404} />;
-
-	const { metadata, contentSections, slug } = page;
-	return (
-		<>
-			{/* Add meta tags for SEO*/}
-			<Seo metadata={metadata} />
-			{/* Display content sections */}
-			<Sections sections={contentSections} slug={slug} />
-		</>
-	);
+	return <>Hello World!</>;
 };
 
 export default Teste;
